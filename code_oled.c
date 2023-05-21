2ea667402f58bc4c7ba89dcea6af7deb1cad072b
andrelambert <rm99148@fiap.com.br>
2023-05-21 11:58:39 -0300

    update code oled

--- code_oled.c
+++ code_oled.c
@@ -1,17 +1,14 @@
-// EDGE COMPUTING - SPRINT 2 - GRUPO 2 (André, Alessandra, Bryan, Vitor Maia e Lucas Feijó)
-
 // Incluir libraries do projeto
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-
 // Definições display OLED
 #define SCREEN_WIDTH 128 
 #define SCREEN_HEIGHT 32 
-#define OLED_RESET     -1 
+#define OLED_RESET -1 
 #define SCREEN_ADDRESS 0x3C 
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // Variáveis para o potenciômetro
@@ -32,12 +29,12 @@ int lastButtonState = HIGH;
 int numActions = 3;
 int action = numActions - 1;
 
-
 void setup() {
   // Inicialização do Display LCD  
-  Wire.begin();  // Inicie a comunicação I2C
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);  // Inicie o display OLED
-  display.clearDisplay();  // Limpe o display OLED
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(2);
   
   // Definição dos pinos
   pinMode(ledPin, OUTPUT);
@@ -53,19 +50,13 @@ void loop() {
   buttonState = digitalRead(buttonPin); 
 
   if (valor_pot != valor_anterior) {
-    display.clearDisplay();
-    display.setCursor(0, 1);
-    display.print("Peso: ");
-    display.print(min(valor_pot, 1000));
-    display.print("g");
-    display.display();
     valor_anterior = valor_pot;
 
     if (valor_pot >= 1000) {
       display.clearDisplay();
       display.setCursor(0, 0);
-      display.print("LIMITE DE PESO");
-      display.setCursor(0, 1);
+      display.print("LIMITE");
+      display.setCursor(0, 16);
       display.print("ATINGIDO");
       display.display();
 
@@ -83,23 +74,28 @@ void loop() {
     }
   }
 
+  // Exibição do peso
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print(min(valor_pot, 1000));
+  display.print("g");
+
   // Lógica do botão
   if (buttonState != lastButtonState && buttonState == LOW) {
     action = (action + 1) % numActions;  // Incrementa a ação do botão
   }
   
-  display.clearDisplay();
-  display.setCursor(0, 0);
+  display.setCursor(0, 16);
   // Exibe o texto do botão de acordo com a ação atual
   switch (action) {
     case 0:
-      display.print("Tipo: Aluminio     ");
+      display.print("Aluminio");
       break;
     case 1:
-      display.print("Tipo: Plastico     ");
+      display.print("Plastico");
       break;
     case 2:
-      display.print("Tipo: Vidro         ");
+      display.print("Vidro");
       break;
     // Adicione mais cases para ações adicionais, se necessário
 
